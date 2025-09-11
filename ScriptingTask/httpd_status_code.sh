796a6155c7f821956ecc28ca735f2d69c3511d94
dsprabhu <samprabhu@gmail.com>
2025-09-11 22:17:26 +0530

    Update httpd_status_code.sh

--- ScriptingTask/httpd_status_code.sh
+++ ScriptingTask/httpd_status_code.sh
@@ -2,12 +2,8 @@
 URL=guvi.in
 status_code=$(curl -o /dev/null -s -w "%{http_code}\n" $URL)
 echo "The site $URL error code is $status_code"
-if [ $status_code -gt 99 -a $status_code -lt 400 ] ; then
+if [ $status_code -gt 199 -a $status_code -lt 300 ] ; then
         echo "The site $URL is sucessfully connected"
-elif [ $status_code -gt 399 -a $status_code -lt 501 ] ; then
-        echo "The site $URL is failed to connect"
-elif [ $status_code -eq 0 ] ; then
-        echo "Recheck the site $URL"
 else
         echo "The site is failed"
 fi
